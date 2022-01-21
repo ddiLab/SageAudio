5cd0ec24b350995646e3203b77edd93a833297ef
Emily Brown <elbrown1800@gmail.com>
2022-01-21 17:38:31 -0600

    first three notebooks completed

---
+++ ColabAI/utils/audio_visualizer.py
@@ -0,0 +1,205 @@
+# -------------------------------------------
+# audio_visualizer.py
+# Helper functions for Audio_Visualizer.ipynb
+# Emily Brown 8/25/2021
+# -------------------------------------------
+
+import numpy as np
+import scipy as sp
+import pandas as pd
+import matplotlib.pyplot as plt
+import ipywidgets as widgets
+from ipywidgets import interact, interactive, fixed, interact_manual
+import IPython.display as ipy
+import os
+import pathlib
+import librosa
+import fma
+import random
+from librosa import display as lbdis
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+import keras
+from keras import layers
+from keras.models import Sequential
+import warnings
+warnings.filterwarnings('ignore')
+
+# --------------------------------------------
+# Public functions
+
+# ---
+# find_songs()
+# Given a path to a folder containing audio files,
+# builds lists of audio files for the notebook to use.
+#
+# Args: path - str of pathname to search.
+# 
+# Returns: song_waves - a list of numpy arrays containing
+#          the waveform of audio files, loaded using Librosa.
+#          song_names - a list containing the names of the audio files,
+#          with the file extensions trimmed.
+#          song_box   - a list of tuples containing the trimmed audio file
+#          names and their index number - used for building interface drop-down box.
+#
+# Note: Currently doesn't check to make sure files are actually audio files - assumes
+#       the folder has been prepared ahead of time.
+
+def find_songs(path):    
+    # Establish input directory
+    input_dir = pathlib.Path(path)
+    if not input_dir.exists():
+        os.mkdir(path)
+    
+    # Search directory and make libraries
+    input_content = os.listdir(input_dir)
+    song_waves = np.empty_like(input_content, dtype=np.ndarray)
+    song_names = np.empty_like(input_content)
+    song_box = np.empty_like(input_content, dtype=tuple)
+
+    cnt = 0
+    for i in input_content:
+        song_waves[cnt] = librosa.load(f'{path}/{i}')
+        song_names[cnt] = i[:-4]
+        song_box[cnt] = (i[:-4], cnt)
+        cnt += 1
+        
+    return [song_waves, song_names, song_box]
+
+# ---
+# visualize()
+# Creates user interface for running visualizations.
+#
+# Args: song_waves - a list of numpy arrays containing audio waveforms.
+#       song_names - a list containing trimmed audio file names.
+#       song_box   - a list of tuples containing the trimmed audio file
+#       names and their index number - used for building interface drop-down box. 
+
+def visualize(song_waves, song_names, song_box):
+    plot_box = ["Wave", "Spectrum", "Spectrogram"]
+    length_box = ["Full", "30 seconds", "5 seconds", "50 milliseconds"]
+
+    song1 = widgets.Dropdown(options=song_box, description="Songs: ")
+    song2 = widgets.Dropdown(options=song_box)
+    song_ui = widgets.HBox([song1, song2])
+    display(song_ui)
+
+    plot1 = widgets.Dropdown(options=plot_box, description="Plots: ")
+    plot2 = widgets.Dropdown(options=plot_box)
+    plot_ui = widgets.HBox([plot1, plot2])
+    display(plot_ui)
+
+    length1 = widgets.Dropdown(options=length_box, description="Length: ")
+    length2 = widgets.Dropdown(options=length_box)
+    length_ui = widgets.HBox([length1, length2])
+    display(length_ui)
+
+    out = widgets.interactive_output(option_interact, {'song_waves': fixed(song_waves), 'song_names': fixed(song_names), 'song1': song1, 'song2': song2, 'plot1': plot1, 'plot2': plot2, 'length1': length1, 'length2': length2})
+    display(out)
+
+# ------------------------------------------
+# Private functions
+
+# ---
+# option_interact()
+# Interactive function called by visualize() public function. Calls select_options()
+# according to interactive specifications and sets appropriate visualization title(s).
+#
+# Args: song_waves - a list of numpy arrays containing audio waveforms.
+#       song_names - a list containing trimmed audio file names.
+#       song1 - index of the first song to plot.
+#       song2 - index of the second song to plot.
+#       plot1 - which type of plot (wave, spectrum, spetrogram) to plot for first song.
+#       plot2 - which type of plot to plot for second song.
+#       length1 - which length (full, 30 seconds, 5 seconds, or 50 milliseconds) to use for first song plot.
+#       length2 - which length to use for second song plot.
+
+def option_interact(song_waves, song_names, song1, song2, plot1, plot2, length1, length2):
+    select_options(song_waves, song1, plot1, length1)
+    plt.title(f"{plot1} plot for \"{song_names[song1]}\" ({length1})")
+    
+    select_options(song_waves, song2, plot2, length2)
+    plt.title(f"{plot2} plot for {song_names[song2]} ({length2})")
+
+# ---
+# select_options()
+# Calls the appropriate plotting functions for a song given a set of options.
+#
+# Args: song_waves - a list of numpy arrays containing audio waveforms.
+#       Song       - index of the song to plot.
+#       Plot       - which type of plot (wave, spectrum, spetrogram) to plot
+#       Length     - which length (full, 30 seconds, 5 seconds, or 50 milliseconds) to use
+
+def select_options(song_waves, Song, Plot, Length):
+    wave = song_waves[Song][0]
+    sr = song_waves[Song][1]
+    
+    if Length == "Full":
+        if Plot == "Wave":
+            plot_wave(wave)
+        elif Plot == "Spectrum":
+            plot_spectrum(wave, sr)
+        elif Plot == "Spectrogram":
+            plot_spectrogram(wave)           
+    else:
+        if Length == "30 seconds":
+            gen_length = 30 * sr
+        if Length == "5 seconds":
+            gen_length = 5 * sr
+        if Length == "50 milliseconds":
+            gen_length = int(0.05 * sr)
+            
+        start_length = 30 * sr
+        if gen_length < len(wave):
+            wave_crop = wave
+            wave_crop = wave_crop[(int(len(wave_crop)/2)-int(gen_length/2)):(int(len(wave_crop)/2)+int(gen_length/2))]
+            if Plot == "Wave":
+                plot_wave(wave_crop)
+            elif Plot == "Spectrum":
+                plot_spectrum(wave_crop, sr)
+            elif Plot == "Spectrogram":
+                plot_spectrogram(wave_crop) 
+        else:
+            select_options(song_waves, Song, Plot, "Full")
+            
+# ---
+# plot_wave()
+# Draws waveform plot using librosa.
+#
+# Args: wave - numpy array of audio waveform.
+
+def plot_wave(wave):
+    plt.figure(figsize=(18,6))
+    lbdis.waveplot(wave, alpha=0.5)
+
+# ---
+# plot_spectrum()
+# Draws spectrum plot. 
+#
+# Args: wave - numpy array of audio waveform.
+#       sr   - sample rate of the audio waveform.
+
+def plot_spectrum(wave, sr):
+    # derive spectrum using FT
+    ft = sp.fft.fft(wave)
+    magnitude = np.absolute(ft)
+    frequency = np.linspace(0, sr, len(magnitude))
+
+    # plot spectrum
+    plt.figure(figsize=(18,6))
+    plt.plot(frequency[:(int(len(frequency)/2))], magnitude[:(int(len(magnitude)/2))]) # magnitude spectrum
+    plt.xlabel("Frequency (Hz)")
+    plt.ylabel("Magnitude")
+
+    
+# ---
+# plot_spectrogram()
+# Draws spectrogram plot using librosa. 
+#
+# Args: wave - numpy array of audio waveform.
+
+def plot_spectrogram(wave):
+    D = librosa.stft(wave)
+    S_db = librosa.amplitude_to_db(np.abs(D), ref=np.max)
+    plt.figure(figsize=(18,6))
+    lbdis.specshow(S_db, x_axis='s')
---
+++ ColabAI/utils/spotify.py
@@ -0,0 +1,224 @@
+# -------------------------------------------
+# spotify.py
+# Helper functions for Spotify.ipynb
+# Emily Brown 8/13/2021
+# -------------------------------------------
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import ipywidgets as widgets
+from ipywidgets import interact, interactive, fixed, interact_manual
+import IPython.display as ipy
+import os
+import pathlib
+import warnings
+warnings.filterwarnings('ignore')
+
+# --------------------------------------------
+# Public functions
+
+# get_tracks()
+# Wrapper for pd.read_csv().
+#
+# Args: path - path of the .csv to read.
+#
+# Returns: pandas dataframe.
+
+def get_tracks(path):
+    return pd.read_csv(path)
+
+
+# search()
+# Creates user interface for running the song search.
+#
+# Args: tracks - dataframe containing song data to search.
+
+def search(tracks):
+    input1 = widgets.Text(
+        description='Song 1:',
+        disabled=False
+    )
+
+    input2 = widgets.Text(
+        description='Song 2:',
+        placeholder="Optional",
+        disabled=False
+    ) 
+    
+    out = interactive(process_query, {'manual': True}, tracks=fixed(tracks), query1=input1, query2=input2)
+    out.children[2].description="Search"
+    display(out)
+
+    
+    
+# ------------------------------------------
+# Private functions   
+    
+# ---
+# process_query()
+# Takes user search strings and querys the track data
+# for it. Hands results of to select_artist() to continue processing
+# results.
+#
+# Args: tracks - full dataset (pandas dataframe)
+#       query1 - string of first song to search
+#       query2 - string of second song to search
+
+def process_query(tracks, query1, query2):
+    queryresults = []
+    for query in (query1, query2):
+        # Check if empty
+        if query == "":
+            queryresults.append("")
+            queryresults.append("")
+        else:
+            # Lower input
+            name = query.lower()
+
+            # Lower track names
+            lowernames = tracks["name"]
+            lowernames = lowernames.str.lower()
+            tracks["name"] = lowernames
+
+            # Lower artist names
+            lowerartists = tracks["artists"]
+            lowerartists = lowerartists.str.lower()
+            tracks["artists"] = lowerartists
+
+            # Search query
+            songresults = tracks[tracks["name"] == name]
+            artistlist = songresults["artists"]
+
+            # Mark empty cases for exceptions
+            if songresults.empty:
+                songresults = ""
+                artistlist = ""
+            
+            # Save results
+            queryresults.append(songresults)
+            queryresults.append(artistlist)
+    
+    # Test for empty results
+    select_artist(queryresults[0], queryresults[1], queryresults[2], queryresults[3])
+
+    
+# ---
+# select_artist()
+# Prepares artists and tracks found in process_query() for plotting.
+# Checks whether a given field was empty / returned no results, and
+# selects the appropriate plotting configuration.
+#
+# Args: results1 - List of tracks found according to the first search.
+#       artists1 - List of artists which had a song matching the first search. 
+#       results2 - List of tracks found according to the second search.
+#       artists2 - List of artists which had a song matching the second search.
+
+def select_artist(results1, artists1, results2, artists2):
+    qflag1 = True
+    qflag2 = True
+    
+    try:
+        if not artists1.empty:
+            artistnames1 = np.empty_like(artists1, dtype=tuple)
+            cnt = 0
+            for i in artists1:
+                artistnames1[cnt] = (format_artist(i), cnt)
+                cnt += 1
+    except:
+        artistnames1 = fixed("")
+        qflag1 = False
+     
+    try:
+        if not artists2.empty:
+            artistnames2 = np.empty_like(artists2, dtype=tuple)
+            cnt = 0
+            for i in artists2:
+                artistnames2[cnt] = (format_artist(i), cnt)
+                cnt += 1
+    except:
+        artistnames2 = fixed("")
+        qflag2 = False       
+    
+    if qflag1 and qflag2:
+        interact(plot_double, results1=fixed(results1), results2=fixed(results2), Artist1=artistnames1, Artist2=artistnames2)
+    elif qflag1:
+        interact(plot_song, results=fixed(results1), Artist=artistnames1)
+    elif qflag2:
+        interact(plot_song, results=fixed(results2), Artist=artistnames2)
+    else:
+        print("No valid results.")
+
+        
+# ---
+# plot_song()
+# Plots spotify feature metrics for a given song.
+#
+# Args: results - List of tracks found according to the search.
+#       Artist - The artist of the specific song to plot.
+
+def plot_song(results, Artist):
+    fig, ax=plt.subplots()
+
+    stats = ('Danceability', 'Energy', 'Speechiness', 'Acousticness', 'Instrumentalness', 'Liveness', 'Valence')
+    values = results.loc[:, ['danceability', 'energy', 'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence']]
+    values = values.iloc[Artist]
+    titlestring = "\"" + (results.iloc[Artist, 1]).title() + "\" by " + ((results.iloc[Artist, 5])[2:-2].replace("\'", "")).title()
+
+    rects = ax.barh(stats, values)
+    ax.invert_yaxis()
+    ax.set_xlabel('Values')
+    ax.set_xlim(left=0.0, right=1.0)
+    ax.bar_label(rects, fmt='%.2f', padding=3)
+    ax.set_title(titlestring)
+    
+    
+# ---
+# plot_double()
+# Plots spotify features for two given songs on a single plot.
+#
+# Args: results1 - List of tracks found according to the first search.
+#       Artist1 - The artist of the first specific song to plot.
+#       results2 - List of tracks found according to the second search.
+#       Artist2 - The artist of the second specific song to plot.
+
+def plot_double(results1, Artist1, results2, Artist2):
+    stats = ('Danceability', 'Energy', 'Speechiness', 'Acousticness', 'Instrumentalness', 'Liveness', 'Valence')
+    values1 = results1.loc[:, ['danceability', 'energy', 'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence']]
+    values1 = values1.iloc[Artist1]
+    title1 = "\"" + (results1.iloc[Artist1, 1]).title() + "\" by " + ((results1.iloc[Artist1, 5])[2:-2].replace("\'", "")).title()
+
+    values2 = results2.loc[:, ['danceability', 'energy', 'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence']]
+    values2 = values2.iloc[Artist2]
+    title2 = "\"" + (results2.iloc[Artist2, 1]).title() + "\" by " + ((results2.iloc[Artist2, 5])[2:-2].replace("\'", "")).title()
+
+    x = np.arange(len(stats))  # the label locations
+    width = 0.35  # the width of the bars
+
+    fig, ax = plt.subplots()
+    rects1 = ax.barh(x - width/2, values1, width, label='Song 1')
+    rects2 = ax.barh(x + width/2, values2, width, label='Song 2')
+
+    # Add some text for labels, title and custom x-axis tick labels, etc.
+    ax.invert_yaxis()
+    ax.set_title(title1 + " vs. " + title2)
+    ax.set_yticks(x)
+    ax.set_yticklabels(stats)
+    ax.legend()
+    ax.set_xlabel('Values')
+    ax.set_xlim(left=0.0, right=1.0)
+    ax.bar_label(rects1, fmt='%.2f', padding=3)
+    ax.bar_label(rects2, fmt='%.2f', padding=3)
+    
+
+# ---
+# format_artist()
+# Given an artist name, formats it for output by removing
+# single quotes and enforcing capitalization.
+#
+# Args: artist - str of artist name to format
+# 
+# Returns: str of formatted artist name
+
+def format_artist(artist):
+    return (artist[2:-2].replace("\'", "")).title() 
